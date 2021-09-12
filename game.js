56c11f8e96ad94bedf8399708c0b1a8fef00cf82
giacovin <giacovin@gmail.com>
2021-09-12 18:53:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ let questions = [];
 
 fetch(
    // 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
-    'https://github.com/giacovin/quickquiz/questions.json'
+    'https://giacovin.github.io/quickquiz/questions.json'
 )
     .then((res) => {
         return res.json();
